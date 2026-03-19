54526088b9feec93c5f7b788a12d27e06ae5742e
bunyim-kong <bunyimkong@gmail.com>
2026-03-19 16:41:44 +0700

    feat: make the structure

---
+++ database/migrations/2026_03_19_015312_create_employees_infor_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees_infor', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('sex', ['male', 'female']);
+            $table->date('dob');
+            $table->string('phone_num');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees_infor');
+    }
+};
